25cec4af2f437f3a18fde263968b5a2452241932
suryasomaraj <99063017+suryasomaraj@users.noreply.github.com>
2022-02-05 10:50:17 +0530

    uploading my project to github respository

---
+++ MyTest/src/packagenew/Child.java
@@ -0,0 +1,13 @@
+package packagenew;
+
+public class Child extends Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Child obj=new Child();
+		obj.add();
+
+	}
+
+}
---
+++ MyTest/src/packagenew/Parent.java
@@ -0,0 +1,13 @@
+package packagenew;
+
+public class Parent {
+	
+	
+	public void add()
+	{
+		System.out.println("ADD");
+	}
+
+	
+
+}
